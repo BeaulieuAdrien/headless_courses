268c1fb97677ee7a5be89a6d11306d97d54b95e6
Adrien Beaulieu <contact@adrienbeaulieu.fr>
2023-12-29 09:08:19 +0100

    init

---
+++ src/api/notation/routes/notation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::notation.notation');
---
+++ src/api/notation/services/notation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notation service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::notation.notation');
